384820bbf9942f7247e1f23b695b7b355f0c5cec
Nishant Khandhar <nishantsk2002@gmail.com>
2025-06-24 20:50:10 -0500

    add: deployment, first release

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com'],
     },
+    output: 'standalone',
+    experimental: {
+        // Enable if you want to use the new app directory features
+        // appDir: true,
+    },
 }
 
 module.exports = nextConfig 
